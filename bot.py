68933b0ee96d80a1bdb7ab2818099dbd595b67cb
SIEEKI 3 <97383131+sieeki@users.noreply.github.com>
2025-10-20 01:39:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,20 @@
 import os
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+import telegram
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 
-# Токен бота
 TOKEN = os.getenv('BOT_TOKEN')
 
 def start(update, context):
-    # Создаем кнопку
-    button = InlineKeyboardButton("get server", callback_data="get_server")
-    keyboard = InlineKeyboardMarkup([[button]])
-    
-    # Отправляем сообщение с кнопкой
-    update.message.reply_text(
-        "Добро пожаловать!\nв данном боте ты можешь генерировать ссылки на приватный сервер!", 
-        reply_markup=keyboard
-    )
+    keyboard = [[telegram.InlineKeyboardButton("get server", callback_data="get_server")]]
+    reply_markup = telegram.InlineKeyboardMarkup(keyboard)
+    update.message.reply_text("Добро пожаловать!\nв данном боте ты можешь генерировать ссылки на приватный сервер!", reply_markup=reply_markup)
 
-def button_click(update, context):
-    # Обрабатываем нажатие кнопки
+def button(update, context):
     update.callback_query.answer()
     update.callback_query.edit_message_text(text="тест")
 
-# Запускаем бота
 updater = Updater(TOKEN)
 updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CallbackQueryHandler(button_click))
-
-print("Бот запущен!")
+updater.dispatcher.add_handler(CallbackQueryHandler(button))
 updater.start_polling()
 updater.idle()
